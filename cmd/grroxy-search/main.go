93ff82d020f58061133cc27485325c97603e47e4
Gitesh Sharma <58145476+glitchedgitz@users.noreply.github.com>
2023-10-04 18:46:52 +0530

    `grroxy-search` first push for grroxy-search

---
+++ cmd/grroxy-search/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/glitchedgitz/grroxy-db/sdk"
+	"github.com/glitchedgitz/grroxy-db/types"
+)
+
+// Define your flags as global variables
+var (
+	isRegexp        bool
+	isCaseSensitive bool
+	isWholeWord     bool
+	search          string
+)
+
+func init() {
+	// Define the flags
+	flag.BoolVar(&isRegexp, "regexp", false, "Treat search string as regular expression")
+	flag.BoolVar(&isCaseSensitive, "caseSensitive", false, "Perform case-sensitive search")
+	flag.BoolVar(&isWholeWord, "wholeWord", false, "Search for whole words only")
+	flag.StringVar(&search, "search", "", "The string or pattern to search for")
+
+	// Parse the flags
+	flag.Parse()
+
+	if search == "" {
+		log.Println("Error: -search flag is required")
+		// Output the usage info
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+var grroxydb *sdk.Client
+var records chan types.ResponseList[map[string]any]
+var wg sync.WaitGroup
+
+func recordsManager() {
+	for {
+		record := <-records
+		for _, r := range record.Items {
+			go searchRecord(r["id"].(string))
+		}
+	}
+}
+
+var CONCURRENT = 50
+
+func main() {
+
+	compileRegex()
+	grroxydb = sdk.NewClient(
+		"http://127.0.0.1:8090",
+		sdk.WithAdminEmailPassword("new@example.com", "1234567890"))
+
+	collection := "_data"
+	sortBy := "-created"
+	filters :=
+		"(original_response.mimetype !~ 'video%' && original_response.mimetype !~ 'audio%' && original_response.mimetype !~ 'image%' && original_response.mimetype !~ 'font%' && original_response.mimetype !~ 'application/font%' && original_response.mimetype !~ 'text/css%') && host ~ 'infojobs' && store_id.response ~ 'candidate-'"
+
+	records = make(chan types.ResponseList[map[string]any])
+	go recordsManager()
+	page := 1
+	total := 0
+	TotalLen := 0
+	for {
+		response, err := grroxydb.List(collection, types.ParamsList{
+			Page: page, Size: CONCURRENT, Sort: sortBy, Filters: filters,
+		})
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		TotalLen = response.TotalItems
+		records <- response
+
+		itemLen := len(response.Items)
+		total += itemLen
+		wg.Add(itemLen)
+		if itemLen < CONCURRENT {
+			break
+		}
+		page += 1
+	}
+
+	wg.Wait()
+
+	log.Println("Total: ", total)
+	log.Println("TotalLen: ", TotalLen)
+}
---
+++ cmd/grroxy-search/search.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+
+	"github.com/glitchedgitz/grroxy-db/sdk"
+)
+
+var field = "response"
+var re *regexp.Regexp
+var err error
+
+func compileRegex() {
+	if !isRegexp {
+		search = regexp.QuoteMeta(search)
+	}
+
+	if !isCaseSensitive {
+		search = "(?i)" + search
+	}
+
+	// If whole word search, add word boundaries (\b) to pattern
+	if isWholeWord {
+		search = `\b` + search + `\b`
+	}
+	log.Println("search", search)
+	re, err = regexp.Compile(search)
+	if err != nil {
+		log.Println("Error compiling regex:", err)
+		return
+	}
+}
+
+func searchRecord(id string) {
+	defer wg.Done()
+	// log.Println(id)
+	collection := sdk.CollectionSet[map[string]any](grroxydb, "_store")
+	data, err := collection.One(id)
+	if err != nil {
+		log.Println("Error reading record:", id, " ", err)
+		return
+	}
+
+	// os.Exit(1)
+	// Compile the regex
+
+	d := data[field].(string)
+	if d != "" {
+		searches := re.FindAllString(d, -1)
+		for _, search := range searches {
+			fmt.Println(search)
+		}
+		// log.Println(id, " : ", searches)
+	}
+
+	if err != nil {
+		log.Println(err)
+	}
+}
