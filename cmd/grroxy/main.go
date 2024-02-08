a35fde1a4b85f5afff3d16c337fece4fb1d801cd
Gitesh Sharma <58145476+glitchedgitz@users.noreply.github.com>
2024-02-08 15:49:18 +0530

    `grroxy` didn't get updated from last change as it was in the `.gitignore`

---
+++ cmd/grroxy/main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	// "github.com/pocketbase/dbx"
+
+	"github.com/glitchedgitz/grroxy-db/api/endpoints"
+	"github.com/glitchedgitz/grroxy-db/config"
+	"github.com/glitchedgitz/grroxy-db/proxy"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+	"github.com/spf13/cobra"
+
+	// "github.com/pocketbase/pocketbase/tools/list"
+	_ "github.com/glitchedgitz/grroxy-db/cmd/grroxy/migrations"
+)
+
+var conf config.Config
+var pb endpoints.DatabaseAPI
+var noUI bool
+var noProxy bool
+var showLogs bool
+var noBanner bool
+
+func serveAndOpen() {
+	if !noProxy {
+		go proxy.StartProxy()
+		// C2.Start()
+	}
+	pb.Serve()
+}
+
+func checkVerbose() {
+	if showLogs {
+		log.SetOutput(os.Stderr)
+	}
+}
+
+func printBanner() {
+	if !noBanner {
+		fmt.Fprint(os.Stderr, banner)
+	}
+}
+
+func init() {
+	log.SetOutput(io.Discard)
+}
+
+func main() {
+	conf.Initiate()
+
+	// Create an instance of the app structure
+	pb = endpoints.DatabaseAPI{
+		App: pocketbase.NewWithConfig(
+			pocketbase.Config{
+				DefaultDataDir:  "grroxy",
+				HideStartBanner: true,
+				// DefaultEncryptionEnv: "hJH#GRJ#HG$JH$54h5kjhHJG#JHG#*&Y&EG#F&GIG@JKGH$JHRGJ##JKJH#JHG",
+			},
+		),
+		Config:     &conf,
+		CmdChannel: make(chan endpoints.RunCommandData),
+	}
+
+	go pb.CommandManager()
+
+	migratecmd.MustRegister(pb.App, pb.App.RootCmd, migratecmd.Config{})
+
+	// Adding custom endpoints
+	pb.App.OnBeforeServe().Add(pb.LabelAttach)
+	pb.App.OnBeforeServe().Add(pb.LabelDelete)
+	pb.App.OnBeforeServe().Add(pb.LabelNew)
+	pb.App.OnBeforeServe().Add(pb.BindFrontend)
+	pb.App.OnBeforeServe().Add(pb.SitemapNew)
+	pb.App.OnBeforeServe().Add(pb.SitemapFetch)
+	pb.App.OnBeforeServe().Add(pb.RunCommand)
+	pb.App.OnBeforeServe().Add(pb.SendRawRequest)
+	pb.App.OnBeforeServe().Add(pb.TextSQL)
+	pb.App.OnBeforeServe().Add(pb.SaveFile)
+	pb.App.OnBeforeServe().Add(pb.ReadFile)
+	pb.App.OnBeforeServe().Add(pb.DownloadCert)
+	pb.App.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		pb.App.Dao().DB().NewQuery(`
+			DELETE FROM _intercept;
+			DELETE FROM tmp_intercept;
+		`).Execute()
+		return nil
+	})
+
+	pb.App.RootCmd.SetHelpTemplate(commandsUsage)
+	pb.App.RootCmd.SetUsageTemplate(commandsUsage)
+
+	// pb.App.RootCmd.PersistentFlags().BoolVar(&noUI, "no-ui", false, "A global flag for the application")
+	pb.App.RootCmd.PersistentFlags().BoolVar(&noProxy, "no-proxy", false, "")
+	pb.App.RootCmd.PersistentFlags().BoolVar(&noBanner, "no-banner", false, "")
+	pb.App.RootCmd.PersistentFlags().BoolVar(&showLogs, "verbose", false, "")
+
+	pb.App.RootCmd.AddCommand(&cobra.Command{
+		Use: "list",
+		Run: func(cmd *cobra.Command, args []string) {
+			printBanner()
+			conf.ListProjects()
+			serveAndOpen()
+			fmt.Println(noUI)
+			checkVerbose()
+		},
+	})
+
+	pb.App.RootCmd.AddCommand(&cobra.Command{
+		Use: ".",
+		Run: func(cmd *cobra.Command, args []string) {
+			conf.OpenCWD()
+			serveAndOpen()
+			checkVerbose()
+		},
+	})
+
+	pb.App.RootCmd.AddCommand(&cobra.Command{
+		Use: "config",
+		Run: func(cmd *cobra.Command, args []string) {
+			conf.ShowConfig()
+			checkVerbose()
+		},
+	})
+
+	pb.App.RootCmd.AddCommand(&cobra.Command{
+		Use: "create",
+		Run: func(cmd *cobra.Command, args []string) {
+			printBanner()
+			projectName := "Project"
+			if len(args) > 0 && args[0] != "." {
+				projectName = strings.Join([]string(args), " ")
+			}
+			conf.NewProject(projectName)
+			serveAndOpen()
+			checkVerbose()
+		},
+	})
+
+	if err := pb.App.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ cmd/grroxy/migrations/initialize.go
@@ -0,0 +1,193 @@
+// Initial setup to start the db file with pre build tables and user
+package migrations
+
+import (
+	"log"
+
+	"github.com/glitchedgitz/grroxy-db/base"
+	"github.com/glitchedgitz/grroxy-db/schemas"
+	"github.com/glitchedgitz/grroxy-db/types"
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+	pbTypes "github.com/pocketbase/pocketbase/tools/types"
+)
+
+type setting struct {
+	ID    string
+	Name  string
+	Value string
+}
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+
+		// you can also access the Dao helpers
+		dao := daos.New(db)
+
+		collection, err := dao.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			log.Println(err)
+		}
+
+		//Delete users
+		if err := dao.DeleteCollection(collection); err != nil {
+			log.Println(err)
+		}
+
+		// create admin
+		admin := &models.Admin{
+			Email:        "new@example.com",
+			PasswordHash: "$2a$13$1EIwr9jv9bJJxfIUd.EtrOGfXCWAm.NuaFt6ZG6OlWmHSUE1Wwdi.",
+		}
+
+		if err := dao.SaveAdmin(admin); err != nil {
+			log.Println(err)
+		}
+
+		// create collections
+		for _, db := range schemas.Collections {
+			collection := &models.Collection{
+				Name:       db.Name,
+				Type:       models.CollectionTypeBase,
+				ListRule:   pbTypes.Pointer(""),
+				ViewRule:   pbTypes.Pointer(""),
+				CreateRule: pbTypes.Pointer(""),
+				UpdateRule: pbTypes.Pointer(""),
+				DeleteRule: nil,
+				Schema:     db.Schema,
+			}
+
+			collection.SetId(db.Name)
+
+			// if db.HasIndex {
+			// 	collection.Indexes = pbTypes.JsonArray[string]{db.Index}
+			// }
+
+			if err := dao.SaveCollection(collection); err != nil {
+				log.Println("[migration][init] Error: ", err)
+			}
+
+			// sites
+
+			log.Println("[migration][init] Creating collectionasdf: ", db.Name)
+		}
+
+		var ind = ""
+		for _, db := range schemas.Collections {
+			ind += db.Index
+			dao.DB().NewQuery(db.Index).Execute()
+		}
+
+		log.Println("[migration][init] Creating Indexes: ", ind)
+
+		// Setting Up Default Settings
+		settingsCollection, err := dao.FindCollectionByNameOrId("_settings")
+		if err != nil {
+			return err
+		}
+
+		settings := []setting{
+			{
+				ID:    base.AddUnderscore("PROJECT_NAME"),
+				Name:  "Project Name",
+				Value: "Untitled Project",
+			},
+			{
+				ID:    base.AddUnderscore("PROXY"),
+				Name:  "Proxy",
+				Value: "127.0.0.1:8080",
+			},
+			{
+				ID:    base.AddUnderscore("INTERCEPT"),
+				Name:  "Intercept",
+				Value: "false",
+			},
+			{
+				ID:    base.AddUnderscore("MAIN_TAB"),
+				Name:  "Main Tab",
+				Value: "Sitemaps",
+			},
+		}
+
+		dao.RunInTransaction(func(txDao *daos.Dao) error {
+			if err != nil {
+				return err
+			}
+
+			for _, val := range settings {
+				record := models.NewRecord(settingsCollection)
+
+				record.Set("id", val.ID)
+				record.Set("option", val.Name)
+				record.Set("value", val.Value)
+
+				if err := dao.SaveRecord(record); err != nil {
+					return err
+				}
+			}
+			return nil
+		})
+		// =================================
+
+		// Setting Up Default Labels
+		labelsCollection, err := dao.FindCollectionByNameOrId("_labels")
+		if err != nil {
+			return err
+		}
+
+		defaultLabels := []types.Label{
+			{Name: "!high", Color: "red", Type: "mark"},
+			{Name: "!medium", Color: "orange", Type: "mark"},
+			{Name: "!low", Color: "yellow", Type: "mark"},
+			{Name: "!info", Color: "ignore", Type: "mark"},
+			{Name: "!leak", Color: "violet", Type: "mark"},
+			{Name: "interesting", Color: "yellow", Type: "custom"},
+			{Name: "weird", Color: "purple", Type: "custom"},
+			{Name: "^dummy/folder", Color: "blue", Type: "folder"},
+			{Name: "^target/reset", Color: "blue", Type: "folder"},
+		}
+
+		dao.RunInTransaction(func(txDao *daos.Dao) error {
+			if err != nil {
+				return err
+			}
+
+			for _, val := range defaultLabels {
+				record := models.NewRecord(labelsCollection)
+				id := base.RandomString(15)
+				record.Set("id", id)
+				record.Set("name", val.Name)
+				record.Set("color", val.Color)
+				record.Set("type", val.Type)
+
+				if err := dao.SaveRecord(record); err != nil {
+					return err
+				}
+
+				collection := &models.Collection{
+					Name:       "label_" + id,
+					Type:       models.CollectionTypeBase,
+					ListRule:   pbTypes.Pointer(""),
+					ViewRule:   pbTypes.Pointer(""),
+					CreateRule: pbTypes.Pointer(""),
+					UpdateRule: pbTypes.Pointer(""),
+					DeleteRule: nil,
+					Schema:     schemas.LabelCollection,
+				}
+
+				if err := dao.SaveCollection(collection); err != nil {
+					log.Println("[migration][creating label collection] Error: ", err)
+				}
+			}
+			return nil
+		})
+
+		return nil
+	}, func(db dbx.Builder) error {
+		// revert changes...
+
+		return nil
+	})
+}
