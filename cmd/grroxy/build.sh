1789c7cae81919351beb4b87dea87de5fc24269a
Gitesh Sharma <58145476+glitchedgitz@users.noreply.github.com>
2024-03-04 21:16:47 +0530

    build.sh update

--- cmd/grroxy/build.sh
+++ cmd/grroxy/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Take version as input
+VERSION=$1
+
+# Check if version is provided
+if [ -z $VERSION ]
+then
+    echo "Version is required"
+    exit 1
+fi
+
 # Define the list of target platforms (OS/ARCH)
 TARGETS=(
     "linux/arm"
@@ -23,7 +33,7 @@ build_binary() {
     echo "Building for $1"
     GOOS=${1%/*}
     GOARCH=${1#*/}
-    GO111MODULE=on GOOS=$GOOS GOARCH=$GOARCH go build -o builds/$BINARY_NAME-$GOOS-$GOARCH
+    GO111MODULE=on GOOS=$GOOS GOARCH=$GOARCH go build -o builds/$VERSION/$BINARY_NAME-$GOOS-$GOARCH
 }
 
 # Loop through each target and build the binary
