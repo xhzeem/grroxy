3bcd14d4abca82de9928cf133e7d66fee345f8d4
Gitesh Sharma <58145476+glitchedgitz@users.noreply.github.com>
2025-06-05 02:52:15 +0530

    deleted repeater endpoint

--- cmd/grroxy/main.go
+++ cmd/grroxy/main.go
@@ -235,7 +235,6 @@ func startCore() {
 	launch.App.OnBeforeServe().Add(launch.API_OpenProject)
 	launch.App.OnBeforeServe().Add(launch.BindFrontend)
 	launch.App.OnBeforeServe().Add(launch.RunCommand)
-	launch.App.OnBeforeServe().Add(launch.SendRawRequest)
 	launch.App.OnBeforeServe().Add(launch.TextSQL)
 	launch.App.OnBeforeServe().Add(launch.SaveFile)
 	launch.App.OnBeforeServe().Add(launch.ReadFile)
