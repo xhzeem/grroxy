a13037f95e89e1cec6f578038eabb16f92634d40
Gitesh Sharma <58145476+glitchedgitz@users.noreply.github.com>
2025-12-08 05:45:13 +0530

    api: `/api/request/add` new param `generated_by`

--- types/api.go
+++ types/api.go
@@ -1,9 +1,10 @@
 package types
 
 type AddRequestBodyType struct {
-	Url        string  `json:"url"`
-	Index      float64 `json:"index"`
-	IndexMinor float64 `json:"index_minor"`
-	Request    string  `json:"request"`
-	Response   string  `json:"response"`
+	Url         string  `json:"url"`
+	Index       float64 `json:"index"`
+	IndexMinor  float64 `json:"index_minor"`
+	Request     string  `json:"request"`
+	Response    string  `json:"response"`
+	GeneratedBy string  `json:"generated_by"`
 }
