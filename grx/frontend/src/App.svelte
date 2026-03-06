88b1a9f6e44325a6112af8058777b7f6929a04ed
Gitesh Sharma <58145476+glitchedgitz@users.noreply.github.com>
2026-03-06 08:36:59 +0530

    frontend v2026.3.3

--- grx/frontend/src/App.svelte
+++ grx/frontend/src/App.svelte
@@ -3,8 +3,8 @@
   import "/node_modules/cybernetic-ui/dist/app.css";
   import "./assets/fonts/fonts.css";
 
-  let host = "../";
-  // let host = "127.0.0.1:8090";
+  // let host = "../";
+  let host = "127.0.0.1:8090";
   // import { GetHost } from "../wailsjs/go/main/App";
   // let host = GetHost();
 </script>
